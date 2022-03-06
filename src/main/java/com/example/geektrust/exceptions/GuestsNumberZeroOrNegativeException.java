bc984d56d76ef3dd01bbf0bd2d804bcfe6d618eb
Mukul Howale <howalemukul@gmail.com>
2022-03-07 02:54:49 +0530

    water management

---
+++ src/main/java/com/example/geektrust/exceptions/GuestsNumberZeroOrNegativeException.java
@@ -0,0 +1,9 @@
+package com.example.geektrust.exceptions;
+
+public class GuestsNumberZeroOrNegativeException extends RuntimeException{
+
+    public GuestsNumberZeroOrNegativeException(String message){
+        super(message);
+    }
+
+}
